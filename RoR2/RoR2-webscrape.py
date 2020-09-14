33f9114aa7c09fc6534d9e8ac1bddb1a9feada7a
David Paez <dpaez013@fiu.edu>
2020-09-14 01:41:45 -0400

    Figuring out how to capture attributes

--- RoR2/RoR2-webscrape.py
+++ RoR2/RoR2-webscrape.py
@@ -16,23 +16,40 @@ def get_links():
     item_links = []
     for row in items_rows:
         for item in row:
-            print(item.a['title'])
             link = "https://riskofrain2.gamepedia.com" + item.a['href']
-            print(link)
             item_links.append(link)
-            print("")
+            # print(item.a['title'])
+            # print(link)
+            # print("")
 
     return item_links
 
-def scrape_info(link):    
+def scrape_info(links): 
+    link = links[0]
     item_link = requests.get(link).text
     item_page = BeautifulSoup(item_link, 'lxml')
 
-    print(item_page.prettify())
+    item_name = item_page.h1.text
+
+    item_box = item_page.find("table", class_="infoboxtable").tbody
+    item_box_rows = item_box.findAll("tr")
+    item_image_link = item_box.img['src']
+    item_quick_desc = item_box.find("td", class_="infoboxdesc")
+
+    print(item_image_link)
+    # item_image_link = item_box_rows[1].a.img['src']
+    # item_small_desc = item_box_rows[2].td.text
+    # item_quick_desc = item_box_rows[3].text
+    # item_rarity = item_box_rows[4].a.text
+    # item_category = item_box_rows[5].text
+    
+
+    # print(item_name + "\n" + item_image_link + "\n" + item_small_desc + "\n" + item_quick_desc 
+    # + "\n" + item_rarity + "\n" + item_category)
 
 def main():
     links = get_links()
-    scrape_info(links[0])
+    scrape_info(links)
 
 main()
 
